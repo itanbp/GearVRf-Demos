e54a5efc5c1c58c9391657e7a227ed54a6dcb8aa
itan <itanbp@gmail.com>
2018-05-08 21:17:03 +0300

    add launcher activity

---
+++ gvr-arcore/app/src/main/java/org/gearvrf/arcore/simplesample/TestActivity.java
@@ -0,0 +1,28 @@
+package org.gearvrf.arcore.simplesample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import org.gearvrf.IViewEvents;
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        ImageView button = (ImageView) findViewById(R.id.test_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TestActivity.this, SampleActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
